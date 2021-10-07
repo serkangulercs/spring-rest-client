c334373d9e4be2a529449fc01225e1e34c2917f9
serkan <serkanguler.cs@gmail.com>
2021-10-07 21:34:01 +0300

    Domain models have been implemented.

---
+++ src/main/java/com/springframework/restclient/api/domain/Name.java
@@ -0,0 +1,47 @@
+package com.springframework.restclient.api.domain;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Name implements Serializable {
+
+    private final static long serialVersionUID = 8066537404755713364L;
+    private String title;
+    private String first;
+    private String last;
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getFirst() {
+        return first;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+    }
+
+    public String getLast() {
+        return last;
+    }
+
+    public void setLast(String last) {
+        this.last = last;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
